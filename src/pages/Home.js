4dcbfb2f3520b7f3c2fc15bbe3f6beddfbf3e48f
Yanni <yannidilly.prog@gmail.com>
2022-12-27 13:44:27 -0300

    adiciona elemento de imagem na secção de introdução

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PhotoSmiling from '../images/personal-pictures/photo-smiling.jpeg';
 import Header from '../components/Header';
 import '../styles/pages/Home.css';
 
@@ -7,14 +8,17 @@ function Home() {
     <div className="home-div">
       <Header />
       <section className="intro-section-home">
-        <h1>
-          Te ajudo a colocar suas finanças
-          {' '}
-          <spam className="text-highlight">pessoais e empresariais</spam>
-          {' '}
-          em ordem...
-        </h1>
-        <h2>você já vai começar a ver a diferença em menos de 30 dias</h2>
+        <div className="intro-text">
+          <h1>
+            Te ajudo a colocar suas finanças
+            {' '}
+            <spam className="text-highlight">pessoais e empresariais</spam>
+            {' '}
+            em ordem...
+          </h1>
+          <h2>você já vai começar a ver a diferença em menos de 30 dias</h2>
+        </div>
+        <img src={ PhotoSmiling } alt="foto Michelle Abritta" />
       </section>
     </div>
   );

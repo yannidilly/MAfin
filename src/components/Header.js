f86aa2f3f3167adb745b17914dd2b63469003942
Yanni <yannidilly.prog@gmail.com>
2022-12-27 12:53:53 -0300

    cria estrutura do Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Logo from '../images/logo.png';
+import '../styles/Header.css';
 
 function Header() {
   return (
-    <h1>header</h1>
+    <header className="header-div">
+      <img className="logo-image-header" src={ Logo } alt="MAfin logo" />
+      <nav>
+        <a href="/">Sobre mim</a>
+        <a href="/">Trabalhos</a>
+        <a href="/">Entrar em contato</a>
+      </nav>
+    </header>
   );
 }
 

211afc40ab0ac7ab9fe8f4228ee823c01db18a05
Yanni <yannidilly.prog@gmail.com>
2022-12-27 13:03:39 -0300

    adiciona estilização ao Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import '../styles/Header.css';
 function Header() {
   return (
     <header className="header-div">
-      <img className="logo-image-header" src={ Logo } alt="MAfin logo" />
-      <nav>
+      <a href="/"><img className="logo-image-header" src={ Logo } alt="MAfin logo" /></a>
+      <nav className="nav-header">
         <a href="/">Sobre mim</a>
         <a href="/">Trabalhos</a>
         <a href="/">Entrar em contato</a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import Header from '../components/Header';
 
 function Home() {
   return (
-    <Header />
+    <div className="home-div">
+      <Header />
+    </div>
   );
 }
 

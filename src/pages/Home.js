39884900fc8c47d885bc7bbf3ca72ae481196b0a
Yanni <yannidilly.prog@gmail.com>
2022-12-27 13:18:45 -0300

    cria highlight no texto de apresentação

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../styles/pages/Home.css';
 
 function Home() {
   return (
     <div className="home-div">
       <Header />
+      <section className="intro-section-home">
+        <h1>
+          Te ajudo a colocar suas finanças
+          {' '}
+          <spam className="text-highlight">pessoais e empresariais</spam>
+          {' '}
+          em ordem...
+        </h1>
+        <h2>você já vai começar a ver a diferença em menos de 30 dias</h2>
+      </section>
     </div>
   );
 }

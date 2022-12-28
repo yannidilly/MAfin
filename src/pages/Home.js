6da5cc281a4d97b0733e5839b05f4252beb016b5
Yanni <yannidilly.prog@gmail.com>
2022-12-28 10:17:07 -0300

    estiliza intro-section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PhotoSmiling from '../images/personal-pictures/photo-smiling.jpeg';
+import PhotoSmiling from '../images/personal-pictures/photo-smiling.png';
 import Header from '../components/Header';
 import '../styles/pages/Home.css';
 
@@ -19,6 +19,9 @@ function Home() {
           <h2>você já vai começar a ver a diferença em menos de 30 dias</h2>
         </div>
         <img src={ PhotoSmiling } alt="foto Michelle Abritta" />
+      </section>
+      <section className="about-section">
+        <p>próxima</p>
       </section>
     </div>
   );
